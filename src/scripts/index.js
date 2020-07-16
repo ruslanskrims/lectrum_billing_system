ebfb4849a42b13ca61b335f991c1ad3411a34e30
Andrey Prisniak <aprisniak@lectrum.io>
2020-07-16 17:41:57 +0300

    feat(source): add examples

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,17 @@
 import '../styles/index.scss';
-import text from './form';
 
-console.log(text);
+let payment = {};
+
+const companies = document.getElementById('companies');
+const meters = document.getElementById('meters');
+
+companies.onclick = (event) => {
+    const id = event.target.getAttribute('data-id');
+
+    payment.id = id;
+};
+
+meters.onchange = (event) => {
+    const { value } = event.target;
+    payment.meterId = value;
+};
