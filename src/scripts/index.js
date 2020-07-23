c0341ab26c73296fe1bf6cb1925e149838776860
Ruslan <krims.ruslan@gmail.com>
2020-07-23 21:34:34 +0300

    Task 1 is done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,22 +1,38 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-import { tarifs } from './constants';
+import { tarifs } from "./constants";
 
 let payments = [];
 let payment = {};
 
-const companies = document.getElementById('companies');
-const meters = document.getElementById('meters');
+const companies = document.getElementById("companies");
+const companyItems = document.querySelectorAll(".left__company");
+const meters = document.getElementById("meters");
+let mainTitle = document.getElementsByClassName("center__title")[0];
+let mainDescription = document.getElementsByClassName("center__desc")[0];
 
-companies.onclick = (event) => {
-    const id = event.target.getAttribute('data-id');
-    const element = document.querySelector(`[data-id=${id}]`);
-    element.style = "background-color: #ccc;";
+//---TASK 1
+companyItems.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    const id = event.target.getAttribute("data-id");
+    const chosenElement = document.querySelector(`[data-id=${id}]`);
 
+    //toggle style solution
+    companyItems.forEach((chosenElement) => {
+      chosenElement.className = "left__company";
+    });
+
+    chosenElement.className = "left__company clicked";
+
+    const main = (mainTitle.innerHTML = chosenElement.textContent);
+    mainDescription.innerHTML = `Payment of the ${main} supply`;
     payment.id = id;
-};
+  });
+});
+
+///-----
 
 meters.onchange = (event) => {
-    const { value } = event.target;
-    payment.meterId = value;
+  const { value } = event.target;
+  payment.meterId = value;
 };
