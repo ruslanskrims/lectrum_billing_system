bc2686f649afcae2618729f37df9caccadac965a
Ruslan <krims.ruslan@gmail.com>
2020-07-24 11:27:26 +0300

    Task 2 is done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,6 +10,8 @@ const companyItems = document.querySelectorAll(".left__company");
 const meters = document.getElementById("meters");
 let mainTitle = document.getElementsByClassName("center__title")[0];
 let mainDescription = document.getElementsByClassName("center__desc")[0];
+const centerForm = document.getElementById("center__form");
+let centerFormFields = centerForm.getElementsByTagName("input");
 
 //---TASK 1
 companyItems.forEach((element) => {
@@ -30,9 +32,38 @@ companyItems.forEach((element) => {
   });
 });
 
+console.log(payment);
 ///-----
+
+///-----TASK 2
 
 meters.onchange = (event) => {
   const { value } = event.target;
   payment.meterId = value;
+  console.log(payment);
 };
+
+centerFormFields.forEach((element) => {
+  element.onchange = (event) => {
+    const { value } = event.target;
+    const id = event.target.getAttribute("id");
+
+    switch (id) {
+      case "previous":
+        payment.previous = value;
+        break;
+      case "current":
+        payment.current = value;
+        break;
+      case "payment":
+        payment.currentOnDate = value;
+        break;
+      default:
+        null;
+        break;
+    }
+    console.log(id);
+    console.log(value);
+    console.log(payment);
+  };
+});
